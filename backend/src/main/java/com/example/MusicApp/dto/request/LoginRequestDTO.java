3b9af207f4640e53255da68e5c6f75bd50c42073
Luxje <binhdath06811@gmail.com>
2026-05-07 20:58:17 +0700

    add LoginRequestDTO, and update AuthServiceImpl

---
+++ backend/src/main/java/com/example/MusicApp/dto/request/LoginRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.MusicApp.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+    @JsonProperty("user_email")
+    private String userEmail;
+
+    @JsonProperty("user_password")
+    private String userPassword;
+}
