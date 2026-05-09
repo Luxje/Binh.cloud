ec136a4ba2620cc29e36e08d13a15ac758d73847
Luxje <binhdath06811@gmail.com>
2026-05-09 16:36:10 +0700

    add new service class for streaming mp3 and wav file

---
+++ backend/src/main/java/com/example/MusicApp/service/StreamingService.java
@@ -0,0 +1,4 @@
+package com.example.MusicApp.service;
+
+public interface StreamingService {
+}
---
+++ backend/src/main/java/com/example/MusicApp/service/impl/StreamingServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.MusicApp.service.impl;
+
+import com.example.MusicApp.service.StreamingService;
+
+public class StreamingServiceImpl implements StreamingService {
+    
+}
